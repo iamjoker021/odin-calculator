c178e65331b2656ce11ad4ac66978bc34bf83781
iamjoker021 <iamjoker021@gmail.com>
2024-07-05 16:01:56 +0530

    Fix backspace key going into Number IF block
    The back being Nan is going inside Number If block instead of Back If block, so changed for correction

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const OPERATORS = ['+', '*', '-', '/'];
-const DIV0_MESSAGE = "Dont divide number by 0";
+const DIV0_MESSAGE = "DivBy0Error, Press AC";
 
 // Create Calculator Layout
 const createKeypadLayout = () => {
@@ -60,7 +60,7 @@ const typeOnDisplay = (buttonValue) => {
     const existingValue = inputDisplay.value;
 
     if (existingValue === DIV0_MESSAGE) {
-        inputDisplay.value = buttonValue;
+        return;
     }
 
     if (buttonValue === ".") {
@@ -68,16 +68,19 @@ const typeOnDisplay = (buttonValue) => {
             inputDisplay.value = existingValue + buttonValue;
         }
     }
-    else if (typeof +buttonValue === 'number') {
+    else if (!isNaN(parseFloat(buttonValue))) {
         if (existingValue == '0') {
-            inputDisplay.value = buttonValue;
+            inputDisplay.value = parseFloat(buttonValue);
         }
         else {
-            inputDisplay.value = existingValue + buttonValue;
+            inputDisplay.value = existingValue + parseFloat(buttonValue);
         }
     }
     else if (OPERATORS.includes(buttonValue)) {
         inputDisplay.value = existingValue + buttonValue;
+    }
+    else if (buttonValue === 'back') {
+        inputDisplay.value = existingValue.slice(0, -1) || '0';
     }
 }
 
@@ -88,10 +91,10 @@ const allClear = () => {
 }
 
 // Backspace Number on Screen
-const backspace = () => {
-    const inputDisplay = document.querySelector('input');
-    inputDisplay.value = inputDisplay.value.slice(0, -1) || '0';
-}
+// const backspace = () => {
+//     const inputDisplay = document.querySelector('input');
+    
+// }
 
 // Is Operator already exist
 const isOperatorAlreadyExist = () => {
@@ -119,7 +122,17 @@ const operate = () => {
     else if (existingValue.includes('-')) {
         result = calculator.subtract(...[existingValue.substring(0, existingValue.lastIndexOf('-')), existingValue.substring(existingValue.lastIndexOf('-') + 1, existingValue.length)].map(parseFloat));
     }
+    console.log(result);
     return result;
+}
+
+const executeEqual = () => {
+    const result = operate();
+    allClear();
+    if (result === DIV0_MESSAGE) {
+        typeOnDisplay(result);
+    }
+    typeOnDisplay(Math.round(result * 10000) / 10000);
 }
 
 // Create Keypad Layout
@@ -131,27 +144,20 @@ keypad.addEventListener('click', (e) => {
 
     const buttonValue = e.target.id;
 
-    if (buttonValue === '.' || buttonValue >= 0 && buttonValue <= 9) {
-        typeOnDisplay(buttonValue);
-    }
-    else if (buttonValue === 'AC') {
+    if (buttonValue === 'AC') {
         allClear();
     }
-    else if (buttonValue === 'back') {
-        backspace();
+    else if (buttonValue === '.' || buttonValue >= 0 && buttonValue <= 9 || buttonValue === 'back') {
+        typeOnDisplay(buttonValue);
     }
     else if (buttonValue === '=') {
         if (isOperatorAlreadyExist()) {
-            const result = operate();
-            allClear();
-            typeOnDisplay(Math.round(result * 10000) / 10000);
+            executeEqual();
         }
     }
     else if (OPERATORS.includes(buttonValue)) {
         if (isOperatorAlreadyExist()) {
-            const result = operate();
-            allClear();
-            typeOnDisplay(Math.round(result * 10000) / 10000);
+            executeEqual();
         }
         typeOnDisplay(buttonValue);
     }
